6b19e2730837d4d15916880895ba1d75474576c9
rosenfence <rosenfence@gmail.com>
2023-05-03 18:09:21 +0900

    chore: Create Project Template

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,9 +1,25 @@
+'use client';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 export default function Projects() {
   return (
     <main className='main'>
       <header className='header'>PROJECTS</header>
-      <section className='section'>내용</section>
+      <section className='section'>
+        <h2>Project Name</h2>
+        <img className='w-[100%]' src='https://s1.zerochan.net/Ootori.Emu.600.3554412.jpg' />
+        <h3>Project Date</h3>
+        <p>
+          Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
     </main>
-    // TODO: 진행해왔던 프로젝트 내용 넣기 : 진행중 / 완료
   );
 }
