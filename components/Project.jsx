df33fa07adccd06d310035c5736d65aab1daddfa
rosenfence <rosenfence@gmail.com>
2023-05-05 23:34:14 +0900

    fix: Project Component 수정

--- components/Project.jsx
+++ components/Project.jsx
@@ -1,6 +1,6 @@
 export default function Project({ name, date, imgSrc, children }) {
   return (
-    <div className='mb-5'>
+    <div className='mb-5 w-[100%]'>
       <h2 className='mb-3 text-3xl font-bold'>{name}</h2>
       <img className='w-[100%] mb-3' src={imgSrc} />
       <h3 className='text-sm text-grey'>{date}</h3>
