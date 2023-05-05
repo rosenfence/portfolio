d46588492d348c5169221bf9fdc5f5217eb3130b
rosenfence <rosenfence@gmail.com>
2023-05-05 23:40:57 +0900

    chore: sample image 수정

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -13,14 +13,19 @@ export default function Projects() {
       <section className='section'>
         <Swiper navigation={true} modules={[Navigation]} className='w-[100%]'>
           <SwiperSlide className='w-[100%]'>
-            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://s1.zerochan.net/Ootori.Emu.600.3554412.jpg'} alt='Project'>
+            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-preview.jpg'} alt='Project'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
               laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
               non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </Project>
           </SwiperSlide>
           <SwiperSlide>
-            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://pbs.twimg.com/media/FY95gyQakAELAS4?format=jpg&name=large'} alt='Project'>
+            <Project
+              name='project name'
+              date='2022.12~2023.01'
+              imgSrc={'https://c4.wallpaperflare.com/wallpaper/531/951/621/digital-digital-art-artwork-illustration-minimalism-hd-wallpaper-preview.jpg'}
+              alt='Project'
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
               laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
               non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
