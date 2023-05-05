b3d52cf81e8c1679a199d08f5f71af3cced35b74
rosenfence <rosenfence@gmail.com>
2023-05-06 00:02:04 +0900

    chore: sample slide 추가 및 pagination 추가

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,17 +1,27 @@
 'use client';
 import Project from '@/components/Project';
 
-import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
+
 import 'swiper/css';
+import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+
+import { Pagination, Navigation } from 'swiper';
 
 export default function Projects() {
   return (
     <main className='main'>
       <header className='header'>PROJECTS</header>
       <section className='section'>
-        <Swiper navigation={true} modules={[Navigation]} className='w-[100%]'>
+        <Swiper
+          pagination={{
+            type: 'progressbar',
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className='w-[100%]'
+        >
           <SwiperSlide className='w-[100%]'>
             <Project name='project name' date='2022.12~2023.01' imgSrc={'https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-preview.jpg'} alt='Project'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
@@ -26,6 +36,27 @@ export default function Projects() {
               imgSrc={'https://c4.wallpaperflare.com/wallpaper/531/951/621/digital-digital-art-artwork-illustration-minimalism-hd-wallpaper-preview.jpg'}
               alt='Project'
             >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Project>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://c4.wallpaperflare.com/wallpaper/133/969/139/artwork-nature-landscape-fantasy-art-wallpaper-preview.jpg'} alt='Project'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Project>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://c4.wallpaperflare.com/wallpaper/586/603/742/minimalism-4k-for-mac-desktop-wallpaper-preview.jpg'} alt='Project'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Project>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://c4.wallpaperflare.com/wallpaper/74/237/143/apple-space-grey-wallpaper-preview.jpg'} alt='Project'>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
               laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
               non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
--- components/Project.jsx
+++ components/Project.jsx
@@ -1,7 +1,7 @@
 export default function Project({ name, date, imgSrc, children }) {
   return (
     <div className='mb-5 w-[100%]'>
-      <h2 className='mb-3 text-3xl font-bold'>{name}</h2>
+      <h2 className='mt-5 mb-3 text-3xl font-bold'>{name}</h2>
       <img className='w-[100%] mb-3' src={imgSrc} />
       <h3 className='text-sm text-grey'>{date}</h3>
       <p>{children}</p>
