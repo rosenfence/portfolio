bff2bdd361d511eca524ff81d4dd2b12ec84a5fd
rosenfence <rosenfence@gmail.com>
2023-05-05 23:03:34 +0900

    feat: Swiper.js 추가

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,28 +1,32 @@
 'use client';
 import Project from '@/components/Project';
 
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 export default function Projects() {
   return (
     <main className='main'>
       <header className='header'>PROJECTS</header>
       <section className='section'>
-        <Project name='project name' date='2022.12~2023.01' imgSrc={'https://s1.zerochan.net/Ootori.Emu.600.3554412.jpg'} alt='Project'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Project>
-        <Project name='project name' date='2022.12~2023.01' imgSrc={'https://pbs.twimg.com/media/FY95gyQakAELAS4?format=jpg&name=large'} alt='Project'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </Project>
+        <Swiper navigation={true} modules={[Navigation]} className='w-[100%]'>
+          <SwiperSlide className='w-[100%]'>
+            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://s1.zerochan.net/Ootori.Emu.600.3554412.jpg'} alt='Project'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Project>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Project name='project name' date='2022.12~2023.01' imgSrc={'https://pbs.twimg.com/media/FY95gyQakAELAS4?format=jpg&name=large'} alt='Project'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+              laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </Project>
+          </SwiperSlide>
+        </Swiper>
       </section>
     </main>
   );
